2c4a393b868c0abd536d976991b55380c369fc67
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-09-20 18:34:31 +0700

    Compact code

--- hello-world/main.c
+++ hello-world/main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 int main() {
-  MPI_Init(NULL, NULL);
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int rank, size, name_len;
   char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Get_processor_name(processor_name, &name_len);
+
   printf("Hello world from processor %s, rank %d out of %d processors\n",
-         processor_name, world_rank, world_size);
+         processor_name, rank, size);
+
   MPI_Finalize();
   return 0;
 }
