739e6a0d6ca35d15b54e92d1a3b9b47336a3c7aa
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-03 19:36:28 +0700

    Fix gather program

--- src/practice-1/gather.cpp
+++ src/practice-1/gather.cpp
@@ -6,35 +6,44 @@ std::string prefix = "[gather] ";
 
 int main(int argc, char **argv) {
   std::cout.setf(std::ios::fixed);
-  int rank, size, sbufsize = 1024;
+  int rank, size, sbufsize = 1024, root = 0;
   char *buf;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   if (rank == 0) {
-    std::cout << prefix + "info: " << size
-              << " processes, root = 0, sbufsize = " << sbufsize << " B\n";
+    std::cout << prefix + "info: " << size << " processes, root = " << root
+              << ", sbufsize = " << sbufsize << " B\n";
   }
 
   double t = MPI_Wtime();
-  if (rank > 0) {  // not root, buf = sbuf
+  if (rank != root) {  // buf = sbuf
     buf = (char *)malloc(sizeof(char) * sbufsize);
     buf[0] = 'a' + rank - 1;
-    MPI_Send(buf, sbufsize, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
-  } else {  // root, buf = rbuf
-    buf = (char *)malloc(sizeof(char) * sbufsize * (size - 1));
+    MPI_Send(buf, sbufsize, MPI_CHAR, root, 0, MPI_COMM_WORLD);
+    std::cout << prefix << rank << ": sent sbuf '" << buf[0] << "' to root\n";
+
+  } else {  // buf = rbuf
+    buf = (char *)malloc(sizeof(char) * sbufsize * size);
+    char *local = (char *)malloc(sizeof(char) * sbufsize);
     MPI_Status status;
-    std::cout << prefix << rank << " (master) is here\n";
-    for (int i = 1; i < size; i++) {
-      MPI_Recv(&buf[(rank - 1) * sbufsize], sbufsize, MPI_CHAR, MPI_ANY_SOURCE,
-               1, MPI_COMM_WORLD, &status);
-      std::cout << prefix << rank << ": received message from "
-                << status.MPI_SOURCE << ": \"" << buf[(rank - 1) * sbufsize]
-                << "\"\n";
-    }
+    MPI_Recv(local, sbufsize, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+             &status);
+    int source = status.MPI_SOURCE;
+    std::copy(&local[0], &local[sbufsize - 1], &buf[(source - 1) * sbufsize]);
+    std::cout << prefix << rank << ": received message from " << source << ": '"
+              << local << "'\n";
   }
   t = MPI_Wtime() - t;
-  std::cout << prefix << rank << ": elapsed " << t << " sec\n";
+
+  if (rank == root) {
+    std::cout << prefix << rank << ": rbuf = \"";
+    for (int i = 0; i < sbufsize * (size - 1); i += sbufsize) {
+      std::cout << buf[i];
+    }
+    std::cout << "\"\n";
+  }
+  std::cout << prefix << rank << ": elapsed " << t << " seconds\n";
 
   free(buf);
   MPI_Finalize();
