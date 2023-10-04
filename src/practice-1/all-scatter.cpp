5c6cd5bb4d477cbfaedfa8f0c6a4782b8d94eed2
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-04 22:18:46 +0700

    Fix unique messages in sbufs of all processes

--- src/practice-1/all-scatter.cpp
+++ src/practice-1/all-scatter.cpp
@@ -14,26 +14,31 @@ int main(int argc, char **argv) {
   char *sbuf = (char *)malloc(sizeof(char) * sbufsize * size);
   for (int i = 0; i < size; ++i) {
     sbuf[i * sbufsize] = 'a' + rank;
+    sbuf[i * sbufsize + 1] = '0' + i;
     rbuf[i * sbufsize] = 'X';
+    rbuf[i * sbufsize + 1] = 'X';
   }
 
   MPI_Request *r = (MPI_Request *)malloc(sizeof(MPI_Request) * size * 2);
   double t = MPI_Wtime();
   for (int i = 0; i < size; ++i) {
     int dst = (rank + i) % size;
-    MPI_Isend(&sbuf[i * sbufsize], sbufsize, MPI_CHAR, dst, 0, MPI_COMM_WORLD, &r[i * 2]);
-    MPI_Irecv(&rbuf[dst * sbufsize], sbufsize, MPI_CHAR, dst, 0, MPI_COMM_WORLD, &r[i * 2 + 1]);
-    std::cout << prefix << "rank " << rank << ": done with rank " << dst << '\n';
+    MPI_Isend(&sbuf[dst * sbufsize], sbufsize, MPI_CHAR, dst, 0, MPI_COMM_WORLD,
+              &r[i * 2]);
+    MPI_Irecv(&rbuf[dst * sbufsize], sbufsize, MPI_CHAR, dst, 0, MPI_COMM_WORLD,
+              &r[i * 2 + 1]);
+    // std::cout << prefix << "rank " << rank
+    //           << ": sent and received from rank " << dst << '\n';
   }
   MPI_Waitall(size * 2, r, MPI_STATUSES_IGNORE);
   t = MPI_Wtime() - t;
 
   std::cout << prefix << "rank " << rank << ": sbuf: ";
   for (int i = 0; i < size; ++i)
-    std::cout << sbuf[i * sbufsize];
+    std::cout << sbuf[i * sbufsize] << sbuf[i * sbufsize + 1] << ' ';
   std::cout << '\n' << prefix << "rank " << rank << ": rbuf: ";
   for (int i = 0; i < size; ++i)
-    std::cout << rbuf[i * sbufsize];
+    std::cout << rbuf[i * sbufsize] << rbuf[i * sbufsize + 1] << ' ';
   std::cout << '\n' << prefix << "rank " << rank << ": time: " << t << '\n';
 
   free(sbuf);
