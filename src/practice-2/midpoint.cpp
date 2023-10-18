789892d61eb0c5db2aca20df610d6d7eb690768b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-18 08:17:12 +0700

    Add MPI implementation for calculating numeric integral with midpoint rule

---
+++ src/practice-2/midpoint.cpp
@@ -0,0 +1,43 @@
+#include <mpi.h>
+
+#include <cmath>
+#include <iostream>
+
+const double eps = 1E-6;
+const int n0 = 100;
+const double a = -1;
+const double b = 1;
+
+double func(double x) {
+  return (sin(x + 2)) / (0.4 + cos(x));
+}
+
+int main(int argc, char **argv) {
+  int commsize, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int n = n0, k;
+  double sq[2], delta = 1;
+  for (k = 0; delta > eps; n *= 2, k ^= 1) {
+    int points_per_proc = n / commsize;
+    int lb = rank * points_per_proc;
+    int ub = (rank == commsize - 1) ? (n - 1) : (lb + points_per_proc - 1);
+    double h = (b - a) / n;
+    double s = 0.0;
+    for (int i = lb; i <= ub; i++) {
+      s += func(a + h * (i + 0.5));
+    }
+    MPI_Allreduce(&s, &sq[k], 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    sq[k] *= h;
+    if (n > n0) {
+      delta = fabs(sq[k] - sq[k ^ 1]) / 3.0;
+    }
+  }
+  if (rank == 0) {
+    std::cout << "[midpoint] result: " << sq[k] << "; Runge rule: EPS = "
+              << eps << ", n = " << n / 2 << '\n';
+  }
+  MPI_Finalize();
+  return 0;
+}
