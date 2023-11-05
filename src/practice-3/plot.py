9ffacde217d764a20c2cfc98143182b6ba9c04ca
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-11-05 11:06:25 +0700

    Rename draw.py to plot.py and move it to practice-3 folder

--- src/practice-3/plot.py
+++ src/practice-3/plot.py
@@ -10,17 +10,18 @@ def draw(filenames, labels, dest_filename):
     plt.rcParams["legend.markerscale"] = 1.0
     plt.rcParams['font.family'] = 'sans-serif'
     plt.rcParams['font.sans-serif'] = ['PT Sans']
-    plt.rcParams['font.size'] = '12'
+    plt.rcParams['font.size'] = '9'
     plt.rcParams["legend.loc"] = "upper left" # 'upper left', 'upper right', 'lower left', 'lower right', 'center'
     plt.rcParams["legend.fontsize"] = "7"
     cm = 1 / 2.54  # centimeters in inches
     fig = plt.figure(figsize=(10*cm, 7*cm))
     ax = fig.add_subplot(111)
     ax.set_title("")
-    ax.set(xlabel="Threads", ylabel="Speedup")
+    #ax.grid()
+    ax.set(xlabel="Number of processes", ylabel="Relative speedup")
     ax.label_outer()
-    #ax.xaxis.set_ticks(np.arange(min(x), max(x)+1, 1))
-    #ax.yaxis.set_ticks(np.arange(min(y), max(y)+1, 1))
+    #ax.xaxis.set_ticks(np.arange(4, 16, 1))
+    #ax.yaxis.set_ticks(np.arange(1.8, 9, 1))
     ax.xaxis.set_tick_params(direction='in', which='both')
     ax.yaxis.set_tick_params(direction='in', which='both')
     #ax.loglog()
@@ -31,9 +32,18 @@ def draw(filenames, labels, dest_filename):
         x = data[:, 0]
         y = data[:, 1]
         #ax.plot(x,y,'-o',markersize=1,c="green")
-        ax.plot(x,y,'-o',markersize=1,linewidth=0.75,label=datalabel)
+        if datalabel != "Linear speedup":
+            if i == 0:
+                marker = '-^'
+                color = "dodgerblue"
+            if i == 1:
+                marker = '-s'
+                color = "orange"
+            ax.plot(x,y,marker,c=color,markersize=3,linewidth=1.0,label=datalabel)
+        else:
+            ax.plot(x,y,'-',c="red",linewidth=1.0,label=datalabel)
         #x.bar(x, y);
-    ax.plot(range(2,9),range(2,9),'-',c="blue",linewidth=0.5,label="Linear speedup")
+        i = 1
 
     plt.tight_layout()
     ax.legend()
@@ -41,4 +51,4 @@ def draw(filenames, labels, dest_filename):
 
 if __name__ == "__main__":
     # draw(["runge.dat"], ["n0 = 1000000"], "runge.png")
-    draw(["mc_1e7.dat", "mc_1e8.dat"], ["n = 10^7", "n = 10^8"], "mc.png")
+    draw(["sgemv_28.dat", "sgemv_45.dat", "linear.dat"], ["N = 28000", "N = 45000", "Linear speedup"], "sgemv.png")
